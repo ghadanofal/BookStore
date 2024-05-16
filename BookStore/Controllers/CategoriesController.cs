f36eb9daedb4b5f322cf77ee91fd64fd465002a2
ghadanofal <181102@ppu.edu.ps>
2024-05-16 22:46:32 +0300

    Display_Category

---
+++ BookStore/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using BookStore.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+	public class CategoriesController : Controller
+	{
+		private readonly ApplicationDbContext context;
+
+		public CategoriesController(ApplicationDbContext context)
+		{
+			this.context = context;
+		}
+		public IActionResult Index()
+		{
+			var result = context.Categories.ToList();
+			return View("Index",result);
+		}
+
+
+	}
+}

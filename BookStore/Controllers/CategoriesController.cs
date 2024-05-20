30ba1e2d7c50c1c4f2a0e1d23fd868fa7f644189
ghadanofal <181102@ppu.edu.ps>
2024-05-20 03:32:44 +0300

    edit/delete

--- BookStore/Controllers/CategoriesController.cs
+++ BookStore/Controllers/CategoriesController.cs
@@ -1,5 +1,8 @@
 ﻿using BookStore.Data;
+using BookStore.Models;
+using BookStore.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace BookStore.Controllers
 {
@@ -17,6 +20,63 @@ namespace BookStore.Controllers
 			return View("Index",result);
 		}
 
+		[HttpGet]
+        public IActionResult Create()
+        {
+            
+            return View("Create");
+        }
 
-	}
+		[HttpPost]
+        public IActionResult Create(CategoryVM categoryVM)
+        {
+			if (!ModelState.IsValid)
+			{
+				return View("Create", categoryVM);
+			}
+
+            var category = new Category
+			{
+				
+				Name = categoryVM.Name
+			};
+            context.Categories.Add(category);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+		[HttpGet]
+		public IActionResult Edit(int id)
+		{
+
+			return View("Create");
+		}
+		[HttpPost]
+		public IActionResult Edit(CategoryVM categoryVM)
+		{
+			var category = context.Categories.Find(categoryVM.Id);
+			if(category == null)
+			{
+				return NotFound();
+			}
+
+			category.Name = categoryVM.Name;
+			context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult Delete(CategoryVM categoryVM)
+		{
+
+			var category = context.Categories.Find(categoryVM.Id);
+			if( category == null )
+			{
+				return NotFound();
+			}
+			context.Categories.Remove(category);
+			context.SaveChanges();
+			return RedirectToAction("Index");
+
+			
+		}
+    }
 }
---
+++ BookStore/ViewModel/CategoryVM.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.ViewModel
+{
+	public class CategoryVM
+	{
+		public int Id { get; set; }
+		[Required(ErrorMessage ="plz insert name")]
+		[MaxLength(35, ErrorMessage ="name should be 35 charachter")]
+		public string Name { get; set; } = null!;
+
+	}
+}
